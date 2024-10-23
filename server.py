15d95183ed89cec2e5a2be72760809a416fd8031
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2024-10-23 21:53:43 +0100

    Template and Elements
    using jinja2 template made elements out of commonly used parts to make it easier to do mass changes and keep the elements the same on all pages

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #Imports
 from flask import Flask
 from threading import Thread
-from flask import render_template
+from flask import render_template, abort
 
 #Setup Variables
 app = Flask(__name__)
@@ -11,9 +11,49 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 #Home Page
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", pageName="Home")
 
-#404 Error
+#About Page
+@app.route("/about")
+def about():
+    abort(404)
+
+#Projects Page
+@app.route("/projects")
+def projects():
+    abort(404)
+
+#Blog Page
+@app.route("/blog")
+def blog():
+    abort(404)
+
+#Contact Page
+@app.route("/contact")
+def contact():
+    abort(404)
+
+#Github Link
+@app.route("/github")
+def github():
+    abort(404)
+
+#Sitemap
+@app.route("/sitemap")
+def sitemap():
+    abort(404)
+
+#Privacy Policy Page
+@app.route("/policies/privacy")
+def privacy_policy():
+    abort(404)
+
+#Cookie Policy Page
+@app.route("/policies/cookies")
+def cookie_policy():
+    abort(404)
+
+#404 Error Page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
