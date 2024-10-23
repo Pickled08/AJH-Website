37b182a5df1f4f9cab21a14a2d10ce0caad8442f
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2024-10-23 20:03:20 +0100

    Home page and 404
    Made a basic homepage with navbar and footer with placeholder text in between.  The 404 page is complete, unless I want something else

--- server.py
+++ server.py
@@ -6,10 +6,17 @@ from flask import render_template
 #Setup Variables
 app = Flask(__name__)
 app = Flask(__name__, template_folder="site_files")
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+#Home Page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#404 Error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 #Functions to run the server
 def run():
