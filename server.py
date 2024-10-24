eb9437d5dd6a6603095a6a2919e4084ee14ac43c
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2024-10-24 02:19:47 +0100

    Registration Page
    Create registration page with a SQLite database and password hashing
    
    Also some footer tweaks

--- server.py
+++ server.py
@@ -4,8 +4,10 @@ from threading import Thread
 from flask import render_template, abort, redirect
 import os
 from dotenv import load_dotenv
-from webforms import LoginForm
+from webforms import LoginForm, RegisterForm
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, UTC
+from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()
 
@@ -17,6 +19,34 @@ app = Flask(__name__)
 app = Flask(__name__, template_folder="site_files")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+#Initialize Database
+db = SQLAlchemy(app)
+
+#Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now(UTC))
+
+    #Password
+    password_hash = db.Column(db.String(128))
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute")
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    #Create A String
+    def __repr__(self):
+        return "<name %r>" % self.name
 
 #Home Page
 @app.route("/")
@@ -64,23 +94,49 @@ def cookie_policy():
     abort(404)
 
 #Register Page
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", pageName="Register")
+    name = None
+    email = None
+    password_hash = None
+    form = RegisterForm()
+    #Validate Form
+    if form.validate_on_submit():
+        email_lc = str.lower(form.email.data)
+        user = Users.query.filter_by(email=email_lc).first()
+        if user is None:
+            #Hash Password
+            hashed_pw = generate_password_hash(form.password_hash.data, "scrypt")
+            user = Users(name=form.name.data, email=email_lc, password_hash=hashed_pw)
+            db.session.add(user)
+            db.session.commit()
+        else:
+            return render_template("register.html", pageName="Register",exists=True, form=form)
+        name = form.name.data
+        email = str.lower(form.email.data)
+        form.name.data = ""
+        form.email.data = ""
+        form.password_hash.data = ""
+        form.password_hash2.data = ""
+    return render_template("register.html", pageName="Register", name=name, email=email, form=form)
 
 #Login Page
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    username = None
+    name = None
     password = None
     form = LoginForm()
     #Validate Form
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        form.username.data = ""
+        name = form.name.data
+        password_hash = form.password_hash.data
+        form.name.data = ""
         form.password.data = ""
-    return render_template("login.html", pageName="Login", username=username, password=password, form=form)
+    return render_template("login.html", pageName="Login", name=name, form=form)
+
+@app.route("/logout")
+def logout():
+    abort(404)
 
 #404 Error Page
 @app.errorhandler(404)
--- webforms.py
+++ webforms.py
@@ -1,10 +1,17 @@
 #Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 #Login Form Class
 class LoginForm(FlaskForm):
-    username = StringField("Username:", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email:", validators=[Email()])
+    password_hash = PasswordField("Password:", validators=[DataRequired()])
     submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[Email()])
+    password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo("password_hash2", message="Passwords do not match!")])
+    password_hash2 =PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
