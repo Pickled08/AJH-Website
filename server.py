edf713cd83fbec985e22e24fed5ce64f8102f21c
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2024-10-24 14:07:59 +0100

    Added Logging in with passwords with flask login

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 #Imports
 from flask import Flask
 from threading import Thread
-from flask import render_template, abort, redirect
+from flask import render_template, abort, redirect, url_for, flash
 import os
 from dotenv import load_dotenv
 from webforms import LoginForm, RegisterForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime, UTC
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 load_dotenv()
 
@@ -20,12 +22,15 @@ app = Flask(__name__, template_folder="site_files")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config['TRAP_HTTP_EXCEPTIONS']=True
+app.config["DEBUG"] = False
 
 #Initialize Database
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #Create Model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
@@ -48,9 +53,19 @@ class Users(db.Model):
     def __repr__(self):
         return "<name %r>" % self.name
 
+#Flask Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
 #Home Page
 @app.route("/")
 def index():
+    flash("Home Page")
     return render_template("index.html", pageName="Home")
 
 #About Page
@@ -102,18 +117,19 @@ def register():
     form = RegisterForm()
     #Validate Form
     if form.validate_on_submit():
-        email_lc = str.lower(form.email.data)
-        user = Users.query.filter_by(email=email_lc).first()
+        email = str.lower(form.email.data)
+        user = Users.query.filter_by(email=email).first()
         if user is None:
             #Hash Password
             hashed_pw = generate_password_hash(form.password_hash.data, "scrypt")
-            user = Users(name=form.name.data, email=email_lc, password_hash=hashed_pw)
+            user = Users(name=form.name.data, email=email, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         else:
             return render_template("register.html", pageName="Register",exists=True, form=form)
         name = form.name.data
         email = str.lower(form.email.data)
+        #Clear Form
         form.name.data = ""
         form.email.data = ""
         form.password_hash.data = ""
@@ -123,25 +139,55 @@ def register():
 #Login Page
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    name = None
+    email = None
     password = None
+    passed = None
+
     form = LoginForm()
     #Validate Form
     if form.validate_on_submit():
-        name = form.name.data
-        password_hash = form.password_hash.data
-        form.name.data = ""
-        form.password.data = ""
-    return render_template("login.html", pageName="Login", name=name, form=form)
+        email = str.lower(form.email.data)
+        password = form.password_hash.data
+        #Clear Form
+        form.email.data = ""
+        form.password_hash.data = ""
 
-@app.route("/logout")
+        #Lookup user by email
+        user = Users.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password_hash, password):
+                login_user(user)
+                flash("Logged In")
+                return redirect(url_for("index"))
+            else:
+                flash("Worng Email or Password")
+        else:
+            flash("Worng Email or Password")
+
+    return render_template("login.html", pageName="Login", form=form, email=email, passed=passed)
+
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
 def logout():
-    abort(404)
+    logout_user()
+    flash("Logged Out Succefully")
+    return redirect(url_for("login"))
+    
 
-#404 Error Page
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+#Error Pages
+@app.errorhandler(Exception)
+def handle_error(e):
+    try:
+        if e.code == 401:
+            return render_template('error_codes/401.html', pageName="401"), 401
+        elif e.code == 404:
+            return render_template('error_codes/404.html', pageName="404"), 404
+        elif e.code == 500:
+            return render_template('error_codes/generic_error.html', pageName="500", errorTitle="Internal Server Error", errorExplain="The server has run into an error trying to load this page"), 500
+        raise e
+    except:
+        return render_template('error_codes/generic_error.html', pageName=e.code, errorTitle="Error", errorExplain="An Error Occurred"), e.code
 
 #Functions to run the server
 def run():
--- webforms.py
+++ webforms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 #Login Form Class
 class LoginForm(FlaskForm):
-    email = StringField("Email:", validators=[Email()])
-    password_hash = PasswordField("Password:", validators=[DataRequired()])
+    email = StringField("Email", validators=[Email()])
+    password_hash = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    name = StringField("Name:", validators=[DataRequired()])
-    email = StringField("Email:", validators=[Email()])
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[Email()])
     password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo("password_hash2", message="Passwords do not match!")])
     password_hash2 =PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Register")
