5e3f81aa880a1ced9e416b74dc0ad5dc928d5bfb
Alex Howard (Pickled08) <alexhoward21@icloud.com>
2025-12-29 13:32:55 +0000

    moved to docker compose

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
-docker stop ajh-website
-git pull
-bash build.sh
-bash run.sh
+#!/bin/bash
+set -e
 
+echo "Pulling latest code..."
+git pull
+
+echo "Building and restarting with Docker Compose..."
+docker-compose up -d --build
+
+echo "Update complete"
